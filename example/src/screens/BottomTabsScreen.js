000968e9c18eafb82b3fc0e30b9b380978c64489
818jsy <818jsy72@gmail.com>
2025-11-27 17:54:13 +0000

    Sync from LiquidGlass-RN repository [skip ci]

--- example/src/screens/BottomTabsScreen.js
+++ example/src/screens/BottomTabsScreen.js
@@ -45,6 +45,7 @@ const BottomTabsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#000000',
   },
   backgroundView: {
     ...StyleSheet.absoluteFillObject,
